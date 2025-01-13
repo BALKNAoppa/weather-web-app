2ca87a5f9a88e23496788925b5b5923dc87f84e3
BALKNAoppa <baterdene207@gmail.com>
2025-01-13 11:56:10 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline hover:text-red-500">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
