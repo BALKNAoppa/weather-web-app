ee52693c469f4c4c002aac03838279af6e3571fb
BALKANA <baterdene207@gmail.com>
2025-01-16 19:07:38 +0800

    icon updates

--- src/components/DaySide.js
+++ src/components/DaySide.js
@@ -11,7 +11,7 @@ export const DaySide = (props) => {
         <div className="DaySide">
             <div className="date">{weather.date}</div>
             <div className="cityname">{weather.cityname}</div>
-            <img className="weather-icon" src={getDayWeatherIcon(weather.codition)}/>
+            <img className="weather-icon" src={getDayWeatherIcon(weather.condition)}/>
             <div className="max-temp">{weather.max_c}°C</div>
             <div className="condition">{weather.condition}</div>
         </div>
--- src/components/NightSide.js
+++ src/components/NightSide.js
@@ -11,7 +11,7 @@ export const NightSide = (props) => {
         <div className="NightSide">
             <div className="date">{weather.date}</div>
             <div className="cityname">{weather.cityname}</div>
-            <img className="weather-icon" src={getNightWeatherIcon(weather.codition)}/>
+            <img className="weather-icon" src={getNightWeatherIcon(weather.condition)}/>
             <div className="min-temp">{weather.min_c}°C</div>
             <div className="condition">{weather.condition}</div>
         </div>
